62cd475cfc6a831c4f0d8f5244d5d918440152ae
Devon Godfrey <devon.hugh.godfrey@gmail.com>
2018-09-19 17:00:51 -0400

    Got a good direction, doing a bit of work

--- clientvideoplayer.sh
+++ clientvideoplayer.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-#Run Scripts and Software
-sh ./refreshvids.sh
-#sh ./clientvideoplayer.sh
-
 # get rid of the cursor so we don't see it when videos are running
 setterm -cursor off
 
 # set here the path to the directory containing your videos
-VIDEOPATH="/home/pi/Desktop/videoslides/live" 
+VIDEOPATH="/home/pi/Desktop/videoslides/client" 
 
 # you can normally leave this alone
 SERVICE="omxplayer"
@@ -20,19 +16,16 @@ count=0
 
 # now for our infinite loop!
 while true; do
-        #if ps ax | grep -v grep | grep $SERVICE > /dev/null 
-        if ! screen -list | grep -q "camera1" | ps ax | grep -v grep | grep $SERVICE > /dev/null 
+        if ps ax | grep -v grep | grep $SERVICE > /dev/null
         then
         sleep .1;
 else
         for entry in $VIDEOPATH/*
         do
                 clear
-                
                 #omxplayer -o hdmi $entry > /dev/null -b
-                #omxplayer --win '0 0 960 720' $entry
-                echo "Playing Video";
-                screen -dmS camera1 sh -c "omxplayer --win '0 0 960 720' $entry"
+                omxplayer --win '960 0 1920 720' $entry
+                #screen -dmS camera1 sh -c "omxplayer --win '0 0 960 720' $entry"
 
 		count=$((count+1))
 		echo $count
@@ -41,7 +34,6 @@ else
 		then
 			echo "we are looping now"
 			count=0
-			sh ./refreshvids.sh
 		fi
         done
 fi
--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#width=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-#height=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-
-#echo width;
 
 dim=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
 
@@ -14,15 +10,11 @@ height="${array[1]}";
 xCen=$((width/2));
 yCen=$((height/2));
 
-#echo $yCen;
-
 chromium-browser file:///home/pi/Desktop/dash/index.html --start-maximized &
 
-screen -dmS camera1 sh -c 'omxplayer --win "0 0 960 540" /home/pi/Desktop/videoslides/buffer/15_late.mp4'
+sleep 5
 
-screen -dmS camera2 sh -c 'omxplayer --win "960 0 1920 540" /home/pi/Desktop/videoslides/buffer/10_missing.mp4'
+screen -dmS camera1 sh -c 'omxplayer --win "0 0 960 720" /home/pi/Desktop/videoslides/buffer/15_late.mp4'
 
-#omxplayer --refresh --win 0,0,640,480 /home/pi/Desktop/videoslides/buffer/15_late.mp4
+screen -dmS camera2 sh -c 'omxplayer --win "960 0 1920 720" /home/pi/Desktop/videoslides/buffer/10_missing.mp4'
 
-
-#omxplayer --refresh --win 100,100,640,480 /home/pi/Desktop/videoslides/buffer/10_missing.mp4
